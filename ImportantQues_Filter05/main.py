a8231da8457977f3a0033174c94ea96633f0acb0
Waghmare Santosh Rajesh <santoshrw03@gmail.com>
2024-07-17 14:06:46 +0530

    NLP based working project which can find most frequently asked questions from previous years

--- ImportantQues_Filter05/main.py
+++ ImportantQues_Filter05/main.py
@@ -1,10 +1,9 @@
-from flask import Flask,request,render_template
-from PyPDF2 import PdfReader,PdfWriter
-import sklearn
+from flask import Flask, request, render_template
+from PyPDF2 import PdfReader
 import numpy as np
 import spacy
 import re
-from spacy.lang.en.stop_words import STOP_WORDS
+import nltk
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -13,19 +12,16 @@ app = Flask(__name__)
 nlp = spacy.load('en_core_web_sm')
 
 def preprocessing(text):
-    text = re.sub('\[\d\]','',text)
-    return " ".join(text)
-def remove_stopwords(sentence):
-    doc = nlp(sentence)
-    return ' '.join([token.text for token in doc if not token.is_stop])
+    text = re.sub(r'\[\d+\]|\d+\]|[a-z]*\]|Q[0-9]*\)|Time :|\[Max.|\d{2}/\d{2}/\d{4}|Q.\d|OR', '', text)
+    text = re.sub(r'\s+', ' ', text)
+    text = text.strip()
+    return text
 
 def generateQues(text):
-    
     questions = []
     doc = nlp(text)
     sentences = [sent.text for sent in doc.sents]
-    cleaned_sentences = [remove_stopwords(sentence) for sentence in sentences]
-    vectorizer = TfidfVectorizer().fit_transform(cleaned_sentences)
+    vectorizer = TfidfVectorizer().fit_transform(sentences)
     vectors = vectorizer.toarray()
     cos_sim_matrix = cosine_similarity(vectors)
     threshold = 0.4
@@ -41,25 +37,21 @@ def generateQues(text):
                 break
         if not added:
             groups.append(set([i, j]))
-    for idx, group in enumerate(groups):
-        # print(f"\nGroup {idx + 1}:")
-        for sentence_idx in group:
-            questions.append(sentences[sentence_idx])
+    grouped_questions = []
+    for group in groups:
+        grouped_questions.append([sentences[sentence_idx] for sentence_idx in group])
+    return grouped_questions
 
-    return questions
-
-#function to process pdf:
 def process_pdf(file):
     text = ""
     pdf_reader = PdfReader(file)
     for page_num in range(len(pdf_reader.pages)):
         page_text = pdf_reader.pages[page_num].extract_text()
-        text+=page_text
+        text += page_text
     return text
 
-@app.route("/",methods = ['POST','GET'])
+@app.route("/", methods=['POST', 'GET'])
 def index():
-
     if request.method == 'POST':
         text = ""
         if 'files[]' in request.files:
@@ -67,16 +59,18 @@ def index():
             for file in files:
                 if file.filename.endswith('.pdf'):
                     text += process_pdf(file)
-                else :
+                else:
                     text += file.read().decode('utf-8')
-
         else:
             text = request.form['text']
-        # cleaned_text = preprocessing(text)
-        questions = generateQues(text)
-        
-        return render_template('questions.html',questions = questions)
 
+        questions = generateQues(text)
+        cleaned_questions = []
+        for group in questions:
+            cleaned_group = [preprocessing(question) for question in group]
+            cleaned_questions.append(cleaned_group)
+
+        return render_template('questions.html', questions=cleaned_questions)
 
     return render_template('index.html')
 
